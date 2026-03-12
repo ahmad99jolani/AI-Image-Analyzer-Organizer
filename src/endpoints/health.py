995f98d49923b068edb415dbcad11d45420c9cf4
Ahmad_Aljolani <ahmadaljolani99@gmail.com>
2026-03-13 02:15:47 +0300

    Enhanced health check file

--- src/endpoints/health.py
+++ src/endpoints/health.py
@@ -4,19 +4,19 @@ from helpers.config import get_settings, Settings
 
 
 pulse = APIRouter(
-    prefix="/test",
-    tags=["image","connection"]
+    prefix="/health",
+    tags=["Health"]
 )
 #app_settings: Settings = get_settings()
 
 @pulse.get('/')
-async def test_connection(app_settings:Settings = Depends(get_settings)):
+async def health_check(app_settings:Settings = Depends(get_settings)):
     return JSONResponse(
         status_code=200,
         content={
-            "status": "alive",
-            "App Name": app_settings.APP_NAME,
-            "App Version": app_settings.APP_VERSION
+            "status": "UP",
+            "app_name": app_settings.APP_NAME,
+            "app_version": app_settings.APP_VERSION,
         }
     )
 
--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI, APIRouter, UploadFile
 from fastapi.responses import JSONResponse
-from endpoints import base, upload
+from endpoints import health, upload
 
 
 app = FastAPI()
 
-app.include_router(base.pulse)
+app.include_router(health.pulse)
 app.include_router(upload.upload)
 
 
